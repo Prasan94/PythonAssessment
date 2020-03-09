7c61e674466b82ba88d0188ba7d6d9fcfcba0b07
Prasan94 <57404270+Prasan94@users.noreply.github.com>
2020-03-09 11:09:47 +0530

    Update db.py

--- src/DB/db.py
+++ src/DB/db.py
@@ -6,17 +6,21 @@ from src.DB.abstract.db import Db as absDb
 class MongoDB(absDb):
     """
     """
-    def __init__(self, uri):
+    def __init__(self, uri, dataBase):
         self.connection = MongoClient(uri)
+        self.db = conn.database #replace with actual db
+        self.collection = db.collection
         
     def __enter__(self):
         return self
     
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_val:
+            print("Error Occurred in DB operation: %s"%exec_val)
         self.connection.close()
 
-    def insert(self):
+    def insert(self, document):
         """
         """
-        pass
+        self.collection.insert_one(document) 
     

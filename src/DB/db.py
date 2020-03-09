9abd027733f7f3271f30868507bc77401aa90ca5
Prasan94 <57404270+Prasan94@users.noreply.github.com>
2020-03-09 10:35:28 +0530

    Update db.py

--- src/DB/db.py
+++ src/DB/db.py
@@ -1,19 +1,23 @@
 """
 """
+from pymongo import MongoClient
 from src.DB.abstract.db import Db as absDb
 
 class MongoDB(absDb):
     """
     """
-    def __init__(self,):
-        pass
-
-    def connect(self):
-        """
-        """
-        pass
+    def __init__(self, uri):
+        self.connection = MongoClient(uri)
+        self.db = conn.database #replace with actual db
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self):
+        self.connection.close()
 
     def insert(self):
         """
         """
         pass
+    
